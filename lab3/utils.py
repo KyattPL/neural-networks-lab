f8958c6cf4bf770fa1ca99f149c97df9425fcbfd
KyattPL <siema1234@gazeta.pl>
2022-11-02 00:28:08 +0100

    Implemented derivatives

--- lab3/utils.py
+++ lab3/utils.py
@@ -5,6 +5,10 @@ def linear(stimulations):
     return stimulations
 
 
+def dx_linear(stimulations):
+    return np.ones(len(stimulations))
+
+
 def softmax(stimulations):
     denominator = 0
 
@@ -26,12 +30,34 @@ def sigmoid(stimulations):
     return activated
 
 
+def sigmoid_single(z):
+    return 1 / (1 + np.power(np.e, -z))
+
+def dx_sigmoid(stimulations):
+    results = []
+    for stim in stimulations:
+        results.append(sigmoid_single(stim) * (1 - sigmoid_single(stim)))
+    
+    return results
+
+
 def hyper_tangent(stimulations):
     activated = []
     for z in stimulations:
         activated.append(-1 + (2 / (1 + np.power(np.e, -2 * z))))
 
     return activated
+
+
+def htan_single(z):
+    return -1 + (2 / (1 + np.power(np.e, -2 * z)))
+
+def dx_hyper_tangent(stimulations):
+    results = []
+    for stim in stimulations:
+        results.append(1 - np.power(htan_single(stim), 2))
+
+    return results
 
 
 def relu(stimulations):
@@ -46,6 +72,50 @@ def relu(stimulations):
     return activated
 
 
+def relu_single(z):
+    return 0 if z < 0 else z
+
+
+def dx_relu(stimulations):
+    results = []
+    for stim in stimulations:
+        if stim > 0:
+            results.append(1)
+        else:
+            results.append(0)
+
+    return results
+
+
+def softplus(stimulations):
+    activations = []
+    for z in stimulations:
+        activations.append(np.log(1 + np.power(np.e, z)))
+
+    return activations
+
+
+def dx_softplus(stimulations):
+    results = []
+    for z in stimulations:
+        results.append(1 / (1 + np.power(np.e, -z)))
+
+    return results
+
+
+def get_derivative(act_fn):
+    if act_fn == linear:
+        return dx_linear
+    elif act_fn == sigmoid:
+        return dx_sigmoid
+    elif act_fn == hyper_tangent:
+        return dx_hyper_tangent
+    elif act_fn == relu:
+        return dx_relu
+    else:
+        return dx_softplus
+
+
 def max_label(outputs):
     maxIndex = 0
     maxPred = outputs[0]
@@ -56,3 +126,11 @@ def max_label(outputs):
             maxIndex = index
     
     return maxIndex
+
+
+def NLL(predictions):
+    results = []
+    for pred in predictions:
+        results.append(-np.log(pred))
+    
+    return results
