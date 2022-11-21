6d9109b366e95843a163b054e85461613c574e81
KyattPL <siema1234@gazeta.pl>
2022-11-21 10:42:56 +0100

    Proper exercise structure

--- lab4/main.py
+++ lab4/main.py
@@ -28,11 +28,8 @@ model = keras.Sequential(
     [
         keras.Input(shape=input_shape),
         layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.MaxPooling2D(pool_size=(2, 2), strides=2),
         layers.Flatten(),
-        layers.Dropout(0.5),
         layers.Dense(num_classes, activation="softmax"),
     ]
 )
