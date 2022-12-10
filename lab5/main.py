25d6dae2b4a048b1ea132704d4ac156b250b2abb
Kajetan Pynka <siema1234@gazeta.pl>
2022-12-10 16:15:44 +0100

    Added experiments

--- lab5/main.py
+++ lab5/main.py
@@ -6,70 +6,128 @@ from sklearn.metrics import confusion_matrix
 from tensorflow import keras
 from keras import layers
 
+BATCH_SIZE = 128
+EPOCHS = 10
+OUT_DIM = 16
+EMBEDDING = 64
+ACTIVATION = 'relu'
+MAX_PAD = 100
+
 # Load the data and split it between train and test sets
-num_words = 15000
-(x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words = num_words)
+maxLens = [50, 80, 100, 130, 150]
 
-maxlen = 130
-x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen)
-x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen)
+for padding in maxLens:
+    num_words = 15000
+    (x_train, y_train), (x_test, y_test) = keras.datasets.imdb.load_data(num_words = num_words)
 
+    x_train = keras.preprocessing.sequence.pad_sequences(x_train, padding)
+    x_test = keras.preprocessing.sequence.pad_sequences(x_test, padding)
 
-rnn = keras.Sequential(
-    [
-        layers.Embedding(num_words, 32, input_length = len(x_train[0])),
-        layers.SimpleRNN(16, input_shape = (num_words, maxlen), return_sequences=False, activation='relu'),
-        layers.Dense(1),
-        layers.Activation('sigmoid')
-    ]
-)
+    rnn = keras.Sequential(
+        [
+            layers.Embedding(num_words, EMBEDDING, input_length = len(x_train[0])),
+            layers.SimpleRNN(OUT_DIM, input_shape = (num_words, padding), return_sequences=False, activation=ACTIVATION),
+            layers.Dense(1),
+            layers.Activation('sigmoid')
+        ]
+    )
 
-batch_size = 128
-epochs = 10
+    lstm = keras.Sequential(
+        [
+            layers.Embedding(num_words, EMBEDDING, input_length=len(x_train[0])),
+            layers.LSTM(OUT_DIM, input_shape=(num_words, padding), return_sequences=False, activation=ACTIVATION),
+            layers.Dense(1),
+            layers.Activation('sigmoid')
+        ]
+    )
 
-rnn.compile(loss="binary_crossentropy",
-                optimizer="rmsprop", metrics=["accuracy"])
+    rnn.compile(loss="binary_crossentropy",
+                    optimizer="rmsprop", metrics=["accuracy"])
 
-model_history = rnn.fit(x_train, y_train, batch_size=batch_size,
-                            epochs=epochs, validation_data=(x_test, y_test))
+    lstm.compile(loss="binary_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
 
-score = rnn.evaluate(x_test, y_test, verbose=0)
+    model_history = rnn.fit(x_train, y_train, batch_size=BATCH_SIZE,
+                                epochs=EPOCHS, validation_data=(x_test, y_test))
 
-print("Test loss:", score[0])
-print("Test accuracy:", score[1])
+    lstm_history = lstm.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_test, y_test))
 
-plt.plot(model_history.history['accuracy'])
-plt.plot(model_history.history['val_accuracy'])
-plt.title('Dokładność modelu - CNN')
-plt.ylabel('precyzja')
-plt.xlabel('epoki')
-plt.legend(['train', 'test'], loc='upper left')
-plt.savefig(f'plots/RNN-acc.png')
+    score = rnn.evaluate(x_test, y_test, verbose=0)
+    score_lstm = lstm.evaluate(x_test, y_test, verbose=0)
 
-plt.clf()
+    print("Test loss:", score[0])
+    print("Test accuracy:", score[1])
 
-plt.plot(model_history.history['loss'])
-plt.plot(model_history.history['val_loss'])
-plt.title('Strata modelu - CNN')
-plt.ylabel('strata')
-plt.xlabel('epoki')
-plt.legend(['train', 'test'], loc='upper left')
-plt.savefig(f'plots/RNN-loss.png')
+    print("Test loss:", score_lstm[0])
+    print("Test accuracy:", score_lstm[1])
 
-plt.clf()
+    plt.plot(model_history.history['accuracy'])
+    plt.plot(model_history.history['val_accuracy'])
+    plt.title('Dokładność modelu - RNN')
+    plt.ylabel('precyzja')
+    plt.xlabel('epoki')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'plots/RNN-acc-padding-{padding}.png')
 
-y_pred = rnn.predict(x_test)
-y_pred = (y_pred > 0.5)
-cm = confusion_matrix(y_test, y_pred)
+    plt.clf()
 
-df1 = pd.DataFrame(columns=["True","False"], index= ["True","False"], data= cm ) 
+    plt.plot(model_history.history['loss'])
+    plt.plot(model_history.history['val_loss'])
+    plt.title('Strata modelu - RNN')
+    plt.ylabel('strata')
+    plt.xlabel('epoki')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'plots/RNN-loss-padding-{padding}.png')
 
-f,ax = plt.subplots(figsize=(3,3))
-sns.heatmap(df1, annot=True,cmap="Blues", fmt= '.0f',ax=ax)
-plt.xlabel("Predykcja etykiety")
-plt.xticks(size = 12)
-plt.yticks(size = 12, rotation = 0)
-plt.ylabel("Prawdziwa etykieta")
-plt.title("Macierz pomyłek", size = 14)
-plt.savefig(f'plots/RNN-conf-matrix.png')
-plt.clf()
+    plt.clf()
+
+    plt.plot(lstm_history.history['accuracy'])
+    plt.plot(lstm_history.history['val_accuracy'])
+    plt.title('Dokładność modelu - LSTM')
+    plt.ylabel('precyzja')
+    plt.xlabel('epoki')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'plots/LSTM-acc-padding-{padding}.png')
+
+    plt.clf()
+
+    plt.plot(lstm_history.history['loss'])
+    plt.plot(lstm_history.history['val_loss'])
+    plt.title('Strata modelu - LSTM')
+    plt.ylabel('strata')
+    plt.xlabel('epoki')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.savefig(f'plots/LSTM-loss-padding-{padding}.png')
+
+    plt.clf()
+
+    y_pred = rnn.predict(x_test)
+    y_pred = (y_pred > 0.5)
+    cm = confusion_matrix(y_test, y_pred)
+
+    df1 = pd.DataFrame(columns=["True","False"], index= ["True","False"], data=cm ) 
+
+    f,ax = plt.subplots(figsize=(3,3))
+    sns.heatmap(df1, annot=True,cmap="Blues", fmt= '.0f',ax=ax)
+    plt.xlabel("Predykcja etykiety")
+    plt.xticks(size = 12)
+    plt.yticks(size = 12, rotation = 0)
+    plt.ylabel("Prawdziwa etykieta")
+    plt.title("Macierz pomyłek", size = 12)
+    plt.savefig(f'plots/RNN-conf-matrix-padding-{padding}.png')
+    plt.clf()
+
+    y_pred = lstm.predict(x_test)
+    y_pred = (y_pred > 0.5)
+    cm = confusion_matrix(y_test, y_pred)
+
+    df1 = pd.DataFrame(columns=["True","False"], index= ["True","False"], data=cm ) 
+
+    f,ax = plt.subplots(figsize=(3,3))
+    sns.heatmap(df1, annot=True,cmap="Blues", fmt= '.0f',ax=ax)
+    plt.xlabel("Predykcja etykiety")
+    plt.xticks(size = 12)
+    plt.yticks(size = 12, rotation = 0)
+    plt.ylabel("Prawdziwa etykieta")
+    plt.title("Macierz pomyłek", size = 12)
+    plt.savefig(f'plots/LSTM-conf-matrix-padding-{padding}.png')
+    plt.clf()
